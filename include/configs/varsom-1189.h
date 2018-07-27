5c9cf61aa4a96529765edee6a95fce5a5b22370a
Aaron Brice <aaron.brice@datasoft.com>
2018-07-27 10:27:36 -0700

    Adding varsom-1189

---
+++ include/configs/varsom-1189.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2018 DataSoft Corp
+ *
+ * Configuration settings specific to the RAP.
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+#ifndef __VARSOM_1189_H
+#define __VARSOM_1189_H
+
+#define FDT_FILE "varsom-1189.dtb"
+
+#include "varsom-common.h"
+
+#endif
