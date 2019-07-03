0de44006f374cec4267b9ea9636588233ed50476
Aaron Brice <aaron.brice@datasoft.com>
2019-07-03 12:39:18 -0700

    Adding varsom-1338

---
+++ include/configs/varsom-1338.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2019 DataSoft Corp
+ *
+ * Configuration settings specific to the Sidebridge Universal with Button.
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+#ifndef __VARSOM_1338_H
+#define __VARSOM_1338_H
+
+#define FDT_FILE "varsom-1338.dtb"
+
+#include "varsom-common.h"
+
+#endif
